8cbecfb3c1f3bc4188f7d27bda772d0263e8d8bf
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-16 05:44:24 -0500

    added ability to get one account

--- PracticeBankManager.Services/AccountService.cs
+++ PracticeBankManager.Services/AccountService.cs
@@ -10,6 +10,13 @@ namespace PracticeBankManager.Services
 {
     class AccountService
     {
+        private readonly Guid _userId;
+
+        public AccountService(Guid userId)
+        {
+            _userId = userId;
+        }
+
         public bool CreateAccount(AccountCreate model)
         {
             var account = new Account()
@@ -25,6 +32,47 @@ namespace PracticeBankManager.Services
                     .Add(account);
 
                 return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public ICollection<AccountListItem> GetAccounts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var accounts =
+                    ctx
+                        .AccountRelationships.Where(ar => ar.UserId == _userId)
+                        .Select(
+                            a => new AccountListItem()
+                            {
+                                AccountId = a.Account.AccountId,
+                                AccountType = a.Account.AccountType,
+                                AccountBalance = a.Account.AccountBalance
+                            });
+
+                return accounts.ToList();
+            }
+        }
+
+        public AccountDetail GetAccount(int accountId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var account =
+                    ctx
+                        .AccountRelationships
+                        .Where(ar => ar.UserId == _userId &&
+                                     ar.AccountId == accountId)
+                        .Select(
+                            a => new AccountDetail()
+                            {
+                                AccountId = a.Account.AccountId,
+                                AccountType = a.Account.AccountType,
+                                AccountBalance = a.Account.AccountBalance
+                            })
+                        .FirstOrDefault();
+
+                return account;
             }
         }
     }
