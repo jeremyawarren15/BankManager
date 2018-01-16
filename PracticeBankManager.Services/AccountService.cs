54ce1e10a859a0907d3e61d75b06a364dc405a4b
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-16 06:25:12 -0500

    refactored create and added delete

--- PracticeBankManager.Services/AccountService.cs
+++ PracticeBankManager.Services/AccountService.cs
@@ -8,7 +8,7 @@ using PracticeBankManager.Models.Account;
 
 namespace PracticeBankManager.Services
 {
-    class AccountService
+    public class AccountService
     {
         private readonly Guid _userId;
 
@@ -25,11 +25,27 @@ namespace PracticeBankManager.Services
                 AccountType = model.AccountType
             };
 
+            if (model.AccountBalance < 0)
+                return false;
+
             using (var ctx = new ApplicationDbContext())
             {
                 ctx
                     .Accounts
                     .Add(account);
+
+                if (ctx.SaveChanges() != 1)
+                    return false;
+
+                var relationship = new AccountRelationship()
+                {
+                    AccountId = account.AccountId,
+                    UserId = _userId
+                };
+
+                ctx
+                    .AccountRelationships
+                    .Add(relationship);
 
                 return ctx.SaveChanges() == 1;
             }
@@ -54,25 +70,84 @@ namespace PracticeBankManager.Services
             }
         }
 
+        private Account GetAccount(ApplicationDbContext context, int accountId)
+        {
+            return
+                context
+                    .AccountRelationships
+                    .Where(ar => ar.UserId == _userId &&
+                                 ar.AccountId == accountId)
+                    .Select(
+                        a => new Account()
+                        {
+                            AccountId = a.Account.AccountId,
+                            AccountType = a.Account.AccountType,
+                            AccountBalance = a.Account.AccountBalance
+                        })
+                    .FirstOrDefault();
+        }
+
         public AccountDetail GetAccount(int accountId)
         {
             using (var ctx = new ApplicationDbContext())
             {
-                var account =
+                var account = GetAccount(ctx, accountId);
+
+                return
+                    new AccountDetail()
+                    {
+                        AccountId = account.AccountId,
+                        AccountType = account.AccountType,
+                        AccountBalance = account.AccountBalance
+                    };
+            }
+        }
+
+        public bool UpdateAccountType(int accountId, string accountType)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var account = GetAccount(ctx, accountId);
+
+                account.AccountType = accountType;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool UpdateAccountBalance(int accountId, decimal changeAmount)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var account = GetAccount(ctx, accountId);
+
+                account.AccountBalance += changeAmount;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteAccount(int accountId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var account = GetAccount(ctx, accountId);
+
+                ctx.Accounts.Remove(account);
+
+                if (ctx.SaveChanges() != 1) return false;
+
+                var accountRelationships = 
                     ctx
                         .AccountRelationships
-                        .Where(ar => ar.UserId == _userId &&
-                                     ar.AccountId == accountId)
-                        .Select(
-                            a => new AccountDetail()
-                            {
-                                AccountId = a.Account.AccountId,
-                                AccountType = a.Account.AccountType,
-                                AccountBalance = a.Account.AccountBalance
-                            })
-                        .FirstOrDefault();
+                        .Where(ar => ar.AccountId == accountId)
+                        .ToList();
 
-                return account;
+                ctx
+                    .AccountRelationships
+                    .RemoveRange(accountRelationships);
+
+                return ctx.SaveChanges() == 1;
             }
         }
     }
