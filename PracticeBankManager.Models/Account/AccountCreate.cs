b76b762047fec2e967c3bc35e0d477078597d4f1
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-16 05:04:02 -0500

    added ability to create account

---
+++ PracticeBankManager.Models/Account/AccountCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeBankManager.Models.Account
+{
+    public class AccountCreate
+    {
+        [DefaultValue(0.00)]
+        public decimal AccountBalance { get; set; }
+
+        [Required]
+        public string AccountType { get; set; }
+    }
+}
---
+++ PracticeBankManager.Services/AccountService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticeBankManager.Data;
+using PracticeBankManager.Models.Account;
+
+namespace PracticeBankManager.Services
+{
+    class AccountService
+    {
+        public bool CreateAccount(AccountCreate model)
+        {
+            var account = new Account()
+            {
+                AccountBalance = model.AccountBalance,
+                AccountType = model.AccountType
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx
+                    .Accounts
+                    .Add(account);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
